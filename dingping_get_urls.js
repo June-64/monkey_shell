c6093783e5905961c5a9ccfcb31e370f7aabec9d
June-64 <473475792@qq.com>
2023-10-16 11:41:44 +0800

    init

---
+++ dingping_get_urls.js
@@ -0,0 +1,117 @@
+// ==UserScript==
+// @name         dianPing get search details page urls
+// @namespace    http://tampermonkey.net/
+// @version      1.3
+// @description  dianPing get search details page urls
+// @author       june
+// @match        *://*.dianping.com/search/keyword/*
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    "use strict";
+    var btnBox = document.createElement("div");
+    btnBox.style.position = "fixed";
+    btnBox.style.bottom = "35px";
+    btnBox.style.right = "10px";
+    btnBox.style.zIndex = "9999";
+  
+    var button = document.createElement("button");
+    button.innerHTML = "下载";
+    button.style.fontSize = "16px";
+    button.style.border = "1px solid black";
+    button.style.backgroundColor = "white";
+    btnBox.appendChild(button);
+  
+    var clearBtn = document.createElement("button");
+    clearBtn.innerHTML = "清空";
+    clearBtn.style.fontSize = "16px";
+    clearBtn.style.border = "1px solid black";
+    clearBtn.style.backgroundColor = "white";
+    clearBtn.style.marginLeft = "10px";
+    btnBox.appendChild(clearBtn);
+    document.body.appendChild(btnBox);
+  
+    clearBtn.addEventListener("click", () => {
+      localStorage.removeItem("urls");
+      toast("清空成功");
+    });
+    button.addEventListener("click", () => {
+      download();
+    });
+    function toast(msg) {
+      var toast = document.createElement("div");
+      toast.style.position = "fixed";
+      toast.style.bottom = "10%";
+      toast.style.right = "50%";
+      toast.style.color = "#fff";
+      toast.style.transform = "translate(50%, 10%)";
+      toast.style.background = "rgba(0,0,0,0.7)";
+      toast.style.borderRadius = "5px";
+      toast.style.padding = "10px";
+      toast.style.display = "none";
+      toast.style.zIndex = "9999";
+      toast.innerHTML = msg;
+      document.body.appendChild(toast);
+      toast.style.display = "block";
+      setTimeout(function () {
+        toast.style.display = "none";
+      }, 2000);
+    }
+  
+    var address = Number(location.href.match(/keyword\/(\d+)/)[1]);
+  
+    function getUrls() {
+      const urls = [];
+      const aList = document.querySelectorAll("li>.pic>a");
+      aList.forEach((item) => urls.push(item.href));
+      let urlsText = localStorage.getItem("urls") || "[]";
+      urlsText = JSON.parse(urlsText);
+      urlsText = urlsText.concat(urls);
+      urlsText = Array.from(new Set(urlsText));
+      toast("当前urls:" + String(urlsText.length));
+      localStorage.setItem("urls", JSON.stringify(urlsText));
+      const nextLink = document.querySelector(".next");
+      // 获取3000-8000之间的随机数
+      let randomNum = Math.floor(Math.random() * 5001) + 3000;
+  
+      let rejectText = document.querySelector("center>h1");
+      if (rejectText !== null) {
+        toast("当前页被拒绝");
+        return;
+      }
+  
+      if (nextLink !== null) {
+        setTimeout(() => {
+          nextLink.click();
+        }, randomNum);
+      } else {
+        var url = location.href;
+        if (!url.includes("keyword")) {
+          return;
+        }
+        address += 1;
+        var newUrl = url
+          .replace(/keyword\/\d+/, `keyword/${address}`)
+          .replace(/p\d+/, "p1");
+        setTimeout(() => {
+          location.href = newUrl;
+        }, randomNum);
+      }
+    }
+  
+    function download() {
+      const urlsText = localStorage.getItem("urls");
+      // Create a download link for the JSON file
+      const downloadLink = document.createElement("a");
+      downloadLink.href =
+        "data:text/json;charset=utf-8," + encodeURIComponent(urlsText);
+      downloadLink.download = "urls.txt";
+      downloadLink.style.display = "none";
+      document.body.appendChild(downloadLink);
+      // Click the download link to download the JSON file
+      downloadLink.click();
+    }
+    getUrls();
+  })();
+  
---
+++ dy_auto.js
@@ -0,0 +1,69 @@
+// ==UserScript==
+// @name         douyin auto scroll
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.douyin.com/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=douyin.com
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    'use strict';
+    const Toast = (message, duration) => {
+        // Create the toast element
+        const toast = document.createElement("div");
+        toast.textContent = message;
+        toast.style.position = "fixed";
+        toast.style.bottom = "20px";
+        toast.style.left = "50%";
+        toast.style.transform = "translateX(-50%)";
+        toast.style.backgroundColor = "black";
+        toast.style.color = "white";
+        toast.style.padding = "10px";
+        toast.style.borderRadius = "5px";
+        toast.style.zIndex = "9999";
+        toast.style.textAlign = "center";
+        document.body.appendChild(toast);
+
+        // Remove the toast after the specified duration
+        setTimeout(function () {
+            toast.parentNode.removeChild(toast);
+        }, duration);
+
+        // Remove the toast when it's clicked
+        toast.addEventListener("click", function () {
+            toast.parentNode.removeChild(toast);
+        });
+    };
+    // 记录上一次的播放进度
+    var progressBefore = 0
+    function getProgress() {
+        const progressBtn = document.querySelector(".xgplayer-playing:not(.xgplayer-pause) .xgplayer-progress-btn");
+        console.log(progressBtn);
+        if (!progressBtn) {
+            document.querySelector(".xgplayer-playswitch-next").click()
+            return
+        }
+        //拿到当前播放进度
+        let point = progressBtn.style.left
+        point = Number.parseFloat(point.replace("%", ''))
+        console.log(point, progressBefore);
+        // 如果当前的播放进度小于上一次的播放进度,说明当前视频已经播放完了
+        if (point < progressBefore) {
+            // 播放下一个
+            document.querySelector(".xgplayer-playswitch-next").click()
+            progressBefore = 0
+            return
+        }
+        // 将当前的进度保存
+        progressBefore = point
+    }
+    setInterval(() => {
+        getProgress()
+    }, 1000);
+
+
+    // Your code here...
+})();
